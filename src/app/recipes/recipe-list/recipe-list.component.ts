990b8acd2b522e83f1aa5db873ca876815986751
Tamer Wagih <mrtamerwagih@gmail.com>
2021-02-05 02:04:42 +0300

    initial Commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Reciepe',
+      'This is Simply a Test',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872'
+    ),
+    new Recipe(
+      'A Test Reciepe',
+      'This is Simply a Test',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872'
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
